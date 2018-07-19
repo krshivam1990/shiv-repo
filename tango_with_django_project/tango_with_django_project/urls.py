f44592e8925fe3ce771c6cfc4e511cea08f31f1b
Shivam <krshivam1990@gmail.com>
2018-07-19 14:44:38 +0530

    Add existing file

---
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -0,0 +1,40 @@
+"""tango_with_django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url, patterns
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+#urlpatterns = [
+#    url(r'^admin/', include(admin.site.urls)),
+#]
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^tango_with_django_project/', include('tango_with_django_project.foo.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
+    #url(r'^static/', include('/static/')), # ADD THIS NEW TUPLE!
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
